22174394a04d6fb76948e8df0ae1aa23742ae73d
burntsaxphonereed <151691868+burntsaxphonereed@users.noreply.github.com>
2023-11-24 20:15:26 -0500

    The resistance

---
+++ script_component.hpp
@@ -0,0 +1,26 @@
+#define COMPONENT iamerica
+#define COMPONENT_BEAUTIFIED iamerica
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_IAMERICA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_IAMERICA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_IAMERICA
+#endif
+#include "\x\\addons\main\script_macros.hpp"
+#define COMPONENT iamerica
+#define COMPONENT_BEAUTIFIED iamerica
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_IAMERICA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_IAMERICA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_IAMERICA
+#endif
+#include "\x\\addons\main\script_macros.hpp"
